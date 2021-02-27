f74eb1d1a28024fdfeb7ecf86730029dbcac3b65
luisandia <luisandiamamani@gmail.com>
2021-02-27 13:57:09 -0500

    initialising react native version

---
+++ react_native_version/components/MovieList.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+interface Props {}
+
+const MovieList = (props: Props) => {
+  return (
+    <div>
+      <Text>Open up App.tsx to start working on your app!</Text>
+    </div>
+  );
+};
+
+export default MovieList;
